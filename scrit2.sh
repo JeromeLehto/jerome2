ec67fc62286c20cd9890a749865486daf5399baa
JeromeLehto <32212736+JeromeLehto@users.noreply.github.com>
2017-10-17 02:09:46 +0000

    Update scrit2.sh

--- scrit2.sh
+++ scrit2.sh
@@ -1,2 +1,10 @@
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git nano
+git clone https://github.com/JeromeLehto/xmr-stak-cpu
+cd xmr-stak-cpu
+cmake .
+make install
+cd bin/
+chmod +x xmr-stak-cpu
+./xmr-stak-cpu
+nano config.txt
