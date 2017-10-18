8754e5721b6dd2a2c26135f55dd957a6ff4a0157
JeromeLehto <32212736+JeromeLehto@users.noreply.github.com>
2017-10-18 00:26:54 +0000

    Update jerome4.sh

--- jerome4.sh
+++ jerome4.sh
@@ -1,14 +1,14 @@
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git nano
-git clone https://github.com/JeromeLehto/xmr-stak-cpu
-cd xmr-stak-cpu
+git clone https://github.com/JeromeLehto/jeromelehto230
+cd jeromelehto230
 cmake .
 make install
 cd bin/
-chmod +x xmr-stak-cpu
-sed -i '114s/.*/"wallet_address" : "4BHUByfCAhmXPusKQB4TGD2WTrCpGZNnUCiNMLWzuMa9isYgVKZAB1W3h4UxcMLWzmfPn2Ymxn82jLGzmCBGvJJxCVzr4ih",/' config.txt
+chmod +x aeon-stak-cpu
+sed -i '114s/.*/"wallet_address" : "WmtMjFJBRUgMod1waafB5WViDj8UnfFv1bbL97e83feSKmDb11LVLBMeWcxnHRVWVhhsS5MNK13zST3Cx3keyjQx1JCnmkgcF",/' config.txt
 sysctl -w vm.nr_hugepages=128
 sudo /bin/sh -c 'echo "vm.nr_hugepages=128" >> /etc/sysctl.conf'
-cd xmr-stak-cpu
+cd aeon-stak-cpu
 cd bin/
-screen ./xmr-stak-cpu
+screen ./aeon-stak-cpu
